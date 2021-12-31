64aff705517d221b3e6da5f7443b884caef49627
schm00g <hk8igureu@relay.firefox.com>
2021-12-31 03:55:23 +0000

    network status

---
+++ src/components/SolanaStatus.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { onMount } from "svelte";
+    import { apiData, networkStatus } from '../store.js';
+    import Info from './Info.svelte';
+
+    let statusData = {};
+    
+    // promise
+
+    onMount(async () => {
+      fetch("https://prod-api.solana.surf/v1/network-status")
+      .then(response => response.json())
+      .then(data => {
+          statusData = data;
+          apiData.set(data);
+      }).catch(error => {
+        console.log(error);
+        return [];
+      });
+    });
+</script>
+    
+<main>
+    {#if statusData}
+        <Info {...statusData}/> 
+    {/if}
+</main>
+    
